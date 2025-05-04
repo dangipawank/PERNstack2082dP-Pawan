a897a4e5bb5d9a9f2349e54ac71f1aacf8662589
dangipawank <dangipawankr2082@gmail.com>
2025-05-04 21:35:56 +0545

    Day12 updates

--- Backend/app.js
+++ Backend/app.js
@@ -1,60 +1,16 @@
 const express = require("express"); // express require gareko
-const { books } = require("./database/connection");
+const bookRoute = require("./routes/bookRoute");
 
 const app = express(); // express lai trigger gareko
 require("./database/connection");
-
-app.use(express.json()); // json data lai accept garna ko lagi
-//test
+app.use(express.json());
 // let app = require("express")()
-app.get("/books", async function (req, res) {
-  // logic to fetch books from database
-  const datas = await books.findAll(); //select * from books
-  res.json({
-    message: "books fetched successfully",
-    datas,
-  });
-});
 
-app.post("/books", async function (req, res) {
-  // logic to add book to database goes here..
-  // console.log(req.body);
-  // const bookName = req.body.bookName;
-  // const authorName = req.body.authorName;
-  const { bookName, authorName, bookPrice, bookGenre } = req.body; // destructuring
-  //  await books.create({
-  //   bookName:bookName,
-  //   authorName:authorName,
-  //   bookPrice:bookPrice,
-  //   bookGenre: bookGenre,
-  // });
-  await books.create({
-    bookName,
-    bookAuthor: authorName,
-    price: bookPrice,
-    bookGenre,
-  });
+// localhost:4000/api+ /hello = localhost:4000/api/hello
+// localhost:4000/api + /books/:id = localhost:4000/api/books/:id
+// localhost:4000/api/haha/ + /books = localhost:4000/api/haha//books
+app.use("/api/books", bookRoute);
 
-  res.json({
-    message: "Book added successfully",
-  });
-});
-
-app.delete("/books/:id", function (req, res) {
-  // logic to delete book
-  res.json({
-    message: "Book Deleted successfully",
-  });
-});
-app.patch("/books/:id", function (req, res) {
-  // logic to update book
-  res.json({
-    message: "Book updated successfully",
-  });
-});
-
-//postgres.scfmicugsxklxgoyqgar:hahahehehuhu@aws-0-ap-south-1.pooler.supabase.com:6543/postgres
-
-postgresql: app.listen(4000, function () {
+app.listen(4000, function () {
   console.log("project/server/backend has started at port 4000");
 });
